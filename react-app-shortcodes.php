6dd6d10d3bf66883312007b6b94e14727428b85e
byeongin yoon <rpf5573@kakao.com>
2024-02-25 20:35:58 +0900

    fix: 예제 수정 - speed prop 추가

--- react-app-shortcodes.php
+++ react-app-shortcodes.php
@@ -7,6 +7,7 @@ shoplic_wp_bridge_react($absoluteDistPath, $localhostUrl)->addShortcode([
     'props' => [
         'object_name' => 'main_slider_props',
         'root_id' => 'main-slider-root-id', // React 컴포넌트를 렌더링할 HTML 요소의 ID
+        'speed' => 500,                                                                
     ],
     'entry_file_name' => 'main-slider/main-slider.tsx', // 엔트리 파일 경로
 ]);
