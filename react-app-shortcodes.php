8886d788a00ec43e41b97bf5f52575b10e6ad752
byeongin <byeongin@shoplic.kr>
2024-03-13 17:42:51 +0900

    Refactor shortcode integration to use updated function signature
    - Modified the function call to `shoplic_wp_bridge_react()` by removing the previously passed parameters `$absoluteDistPath` and `$localhostUrl` directly in the function call.
    - Added `absolute_dist_path` and `localhost_url` to the shortcode's `props` array to include the absolute path to the React component bundle file and the development server URL, aligning with the updated function usage.

--- react-app-shortcodes.php
+++ react-app-shortcodes.php
@@ -2,9 +2,11 @@
 $absoluteDistPath = get_template_directory() . '/react-app/dist';
 $localhostUrl = 'http://localhost:5173';
 
-shoplic_wp_bridge_react($absoluteDistPath, $localhostUrl)->addShortcode([
+shoplic_wp_bridge_react()->addShortcode([
     'shortcode_name' => 'main_slider', // shortcode 이름
     'props' => [
+        'absolute_dist_path' => $absoluteDistPath, // React 컴포넌트 번들 파일의 절대 경로
+        'localhost_url' => $localhostUrl, // 개발 서버 URL
         'object_name' => 'main_slider_props',
         'root_id' => 'main-slider-root-id', // React 컴포넌트를 렌더링할 HTML 요소의 ID
         'speed' => 500,                                                                
